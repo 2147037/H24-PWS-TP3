175a48236cb03e7a03834acdcb7edaa1970ef650
Jin <2147037@cegepmontpetit.ca>
2024-03-27 13:35:11 -0400

    FCT Modification des requêtes

--- TP3/flappyBirb_depart/src/app/services/MyService.service.ts
+++ TP3/flappyBirb_depart/src/app/services/MyService.service.ts
@@ -14,13 +14,13 @@ constructor(public http :HttpClient) { }
   async register(username : string, email : string , pw : string, pwConfirm : string) : Promise<void>{
     let registerDTO = new RegisterDTO(username, email, pw, pwConfirm)
 
-    let x = await lastValueFrom(this.http.post<any>("https://localhost:7055/api/Users/Register", registerDTO));
+    let x = await lastValueFrom(this.http.post<any>("http://localhost:5221/api/Users/Register", registerDTO));
     console.log(x);
   }
 
   async login(username : string, pw : string) : Promise<void>{
     let loginDTO = new LoginDTO(username, pw);
-    let x = await lastValueFrom(this.http.post<any>("https://localhost:7055/api/Users/Login", loginDTO));
+    let x = await lastValueFrom(this.http.post<any>("http://localhost:5221/api/Users/Login", loginDTO));
     console.log(x);
     localStorage.setItem("token", x.token);
 
